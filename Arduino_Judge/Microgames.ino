7902c25dcef64891e62a3081178736df6735d3d4
Daniel Barlow <djb81@bath.ac.uk>
2015-11-27 14:45:48 +0000

    Added Games
    Added the LED number and LED brightness Games

--- Arduino_Judge/Microgames.ino
+++ Arduino_Judge/Microgames.ino
@@ -6,6 +6,8 @@
  */
 
 GameResult runMicrogame(GameID game, byte myNumber, byte otherNumber) {
+  lowerButtonPressed = false;
+  higherButtonPressed = false;
   millisAtGameStart = millis();
   
   switch (game) {
@@ -48,14 +50,87 @@ GameResult runPiezoRhythm(byte myNumber, byte otherNumber) {
 
 GameResult runLEDNumber(byte myNumber, byte otherNumber) {
   // LED - 4-bit highest number
-  // TODO: Must write this microgame!
-  return GameTied;
+  GameResult gameState = GameTied;
+  int shift = myNumber + 1
+  //Work out which LEDs to light up.
+  for (int i = 3; i >= 0;i--){
+    //If the right most bit is a one, then shift%2 is 1, so light up the LED.
+    if (shift%2 == 1){
+      digitalWrite(fourBitLEDs[i],HIGH);
+    }
+    else{
+      digitalWrite(fourBitLEDs[i],LOW);
+    }
+    shift = shift >> 1
+  }
+  //Keep going until the game is no longer tied.
+  while (gameState == GameTied){
+    if (lowerButtonPressed){
+      //Were they corrrect to push the dodge button?
+      if (myNumber < otherNumber){
+        //yes
+        gameState = CorrectDodge;
+      }
+      else{
+        //No
+        gameState = IncorrectDodge;
+      }
+    }
+    else if{higherButtonPressed){
+      //Were they right to push the attack button?
+      if (myNumber < otherNumber){
+        //no
+        gameState = IncorrectAttack;
+      }
+      else{
+        //yes
+        gameState = CorrectAttack;
+      }
+    }
+    //Comunicate the current game state.
+    GameResult communicatedGameState = communicateGameStatus(gameState);
+    if (communicatedGameState != GameTied){
+      return communicatedGameState;
+    }
+  }
 }
 
 GameResult runLEDBrightest(byte myNumber, byte otherNumber) {
   // LED - brightest
-  // TODO: Must write this microgame!
-  return GameTied;
+  int brightness = 55 + myNumber*40;
+  GameResult gameState = GameTied;
+  analogWrite(analogLED, brightness);
+  //Keep going until the game is no longer tied.
+  while (gameState == GameTied){
+    if (lowerButtonPressed){
+      //Were they corrrect to push the dodge button?
+      if (myNumber < otherNumber){
+        //yes
+        gameState = CorrectDodge;
+      }
+      else{
+        //No
+        gameState = IncorrectDodge;
+      }
+    }
+    else if{higherButtonPressed){
+      //Were they right to push the attack button?
+      if (myNumber < otherNumber){
+        //no
+        gameState = IncorrectAttack;
+      }
+      else{
+        //yes
+        gameState = CorrectAttack;
+      }
+    }
+    //Comunicate the current game state.
+    GameResult communicatedGameState = communicateGameStatus(gameState);
+    if (communicatedGameState != GameTied){
+      digitalWrite(analogLED,LOW);
+      return communicatedGameState;
+    }
+  }
 }
 
 GameResult runLEDFrequency(byte myNumber, byte otherNumber) {
@@ -67,18 +142,23 @@ GameResult runLEDFrequency(byte myNumber, byte otherNumber) {
 GameResult runLDRCover() {
   // LDR - first to cover
   digitalWrite(whiteLED,HIGH);
-  delay(100);
+  delay(100);//Reaction time, this just lets the LED turn on and if players are reacting faster then this, they are cheeting anyway.
+  //Obtain the initial value of the sensor pin
   int sensorValue = analogRead(LDRPin);
   GameResult gameState = GameTied;
+  //Keep going until the game is no longer tied.
   while (gameState == GameTied){
-    //Darkness is a high value, well lit is a low value.
     int newValue = analogRead(LDRPin);
+    //Cheek to see if there is a noticable increase(=less light) over the LED.
     if (newValue>=sensorValue + 300 || newValue==1023){
+      //Change game state to correct attack and record the time of this.
       millisAtButtonPress = millis(); // Added this, since it's necessary for resolving conflicts!
       gameState = CorrectAttack;
     }
+    //Comunicate the current game state.
     GameResult communicatedGameState = communicateGameStatus(gameState);
     if (communicatedGameState != GameTied){
+      digitalWrite(whiteLED,LOW);
       return communicatedGameState;
     }
   }
